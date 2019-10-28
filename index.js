da12cf9198226fe6582094325798c576678e715e
Alexandr Rudenskyi <r-@bk.ru>
2019-10-28 19:15:07 +0200

    19-10-28

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+/*
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+*/
 
-const PORT = process.env.PORT || 5000;
+const PORT = 80;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
---
+++ public/script/main.js
@@ -0,0 +1,60 @@
+function dodlladdresscopy(id) {
+  let copyText = document.getElementById(id);
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+
+  let nel = document.getElementById('notification');
+  let ntxt = document.getElementById('notification_text');
+  ntxt.innerHTML = `<b>'${copyText.value}'</b> copied to clipboard`;
+  nel.style = 'display: block;';
+  setTimeout(() => {
+    nel.style = 'display: none;';
+  }, 3000);
+}
+
+function setGenBalanceText() {
+  let xhr = new XMLHttpRequest();
+  let url = `https://nodes.wavesnodes.com/addresses/balance/details/3PLp1QsFxukK5nnTBYHAqjz9duWMriDkHeT`;
+
+  xhr.open("GET", url);
+  xhr.send(null);
+
+  xhr.onload = function () {
+    function fixed3(x) {
+      if (x < 10) {
+        return `00${x}`;
+      }
+      else if(x < 100){
+        return `0${x}`;
+      }
+      return x;
+    }
+
+    let generating = Math.floor(((JSON.parse(xhr.response)).generating) / 100000000);
+    let el = document.getElementById('gen_balance_num');
+    let k6 = Math.floor(generating / 1000000);
+    let k3 = Math.floor((generating - k6 * 1000000) / 1000);
+    let k1 = Math.floor(generating - k3 * 1000 - k6 * 1000000);
+    if (k3 > 0) {
+      k1 = fixed3(k1);
+    }
+    if (k6 > 0) {
+      k3 = fixed3(k3);
+    }
+
+    el.innerHTML = `${k6},${k3},${k1}`;
+  };
+
+  /*
+    if (xhr.status === 200) {
+      return xhr.responseText;
+    }
+    else {
+      return null;
+    }*/
+}
+
+window.onload = (event) => {
+  setGenBalanceText();
+};
