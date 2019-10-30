3f0948ffbc9df8512811551b64f60db447ba473e
Alexandr Rudenskyi <r-@bk.ru>
2019-10-30 20:57:04 +0200

    19-10-30B

--- public/script/main.js
+++ public/script/main.js
@@ -27,9 +27,14 @@ function setGenBalanceText() {
 
   xhr.onload = function () {
     let el = document.getElementById('gen_balance_num');
+    let elm = document.getElementById('mined_num');
     let generating = Math.floor(((JSON.parse(xhr.response)).generating) / 100000000);
+    let mined = Math.floor(((JSON.parse(xhr.response)).available) / 100000000);
     el.innerHTML = new Intl.NumberFormat("jp-JP",{minimumIntegerDigits: 1, minimumFractionDigits: 0, maximumFractionDigits: 0}).format(generating);
+    elm.innerHTML = new Intl.NumberFormat("jp-JP",{minimumIntegerDigits: 1, minimumFractionDigits: 0, maximumFractionDigits: 0}).format(mined);
     document.getElementById('gen_balance_caption').innerHTML = 'Generating Balance';
+    document.getElementById('mined_txt').innerHTML = 'WAVES mined';
+    document.getElementById('mined_txt2').innerHTML = 'from last payout';
   };
 }
 
