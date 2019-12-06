76870fd33fec8511d68ef7be40ba63ee51a4c003
Alexandr Rudenskyi <r-@bk.ru>
2019-12-06 23:53:42 +0200

    19-12-06-2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const request = require('request');
 
 const app = express();
 
@@ -9,6 +10,31 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 */
+
+app.get('/vote_status', (req, res) => {
+    request(
+        { url: 'http://144.91.84.150:1250/vote_status' },
+        (error, response, body) => {
+          if (error || response.statusCode !== 200) {
+            return res.status(500).json({ type: 'error', message: err.message });
+          }
+          res.json(JSON.parse(body));
+        }
+      )
+});
+
+app.get('/vote_status/:adr', (req, res) => {
+  let adr = req.params.adr;
+  request(
+    { url: `http://144.91.84.150:1250/vote_status/${adr}` },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: err.message });
+      }
+      res.json(JSON.parse(body));
+    }
+  )
+});
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
